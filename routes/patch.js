a63ecef2e52c580b28e2afe66aa84456213d6bd7
Jayant Arora <jaistudy1996@gmail.com>
2017-04-07 12:24:20 -0400

    Fix throwing errors instead just log them on console. Will keep the app running and will rollback any changes

--- routes/patch.js
+++ routes/patch.js
@@ -17,8 +17,10 @@ router.post('/patchFiber', function(req, res, next){
 	console.log(req.body);
 	db.query('INSERT INTO patch_fiber (to_strand_id, from_strand_id, patch_cable, from_type, to_type) values (?, ?, ?, ?, ?)', [req.body.to_strand_id, req.body.from_strand_id, req.body.patch_cable, req.body.from_type, req.body.to_type], function(error, results){
 		if(error){
-			console.log(error);
-			throw error;
+			db.rollback(function(){
+				console.log(error);
+			});
+			//throw error;
 		}
 		if(results.insertId){
 			console.log(results.insertId);
@@ -26,8 +28,9 @@ router.post('/patchFiber', function(req, res, next){
 			if(req.body.from_type == 'Switch'){
 				db.query('INSERT INTO patch_switch_fiber (patch_id, ip_address, port_no) values(?, ?, ?)', [results.insertId, req.body.from_ip_address, req.body.from_port_numnber], function(error, results){
 					if(error){
-						console.log(error);
-						throw error;
+						db.rollback(function(){
+							console.log(error);
+						});
 					}
 					return;
 				});
@@ -35,8 +38,9 @@ router.post('/patchFiber', function(req, res, next){
 			if(req.body.to_type == 'Switch'){
 				db.query('INSERT INTO patch_switch_fiber (patch_id, ip_address, port_no) values(?, ?, ?)', [results.insertId, req.body.to_ip_address, req.body.to_port_numnber], function(error, results){
 					if(error){
-						console.log(error);
-						throw error;
+						db.rollback(function(){
+							console.log(error);
+						});
 					}
 					return;
 				});
@@ -44,8 +48,9 @@ router.post('/patchFiber', function(req, res, next){
 			if(req.body.from_type == 'FiberEnclosure'){
 				db.query('INSERT INTO patch_FE_fiber (patch_id, enclosure_no, panel_no, port_no) values (?, ?, ?, ?)', [results.insertId, req.body.from_enclosure_number, req.body.from_panel_no, req.body.from_port_number], function(error, results){
 					if(error){
-						console.log(error);
-						throw error;
+						db.rollback(function(){
+							console.log(error);
+						});
 					}
 					return;
 				});
@@ -53,8 +58,9 @@ router.post('/patchFiber', function(req, res, next){
 			if(req.body.to_type == 'FiberEnclosure'){
 				db.query('INSERT INTO patch_FE_fiber (patch_id, enclosure_no, panel_no, port_no) values(?, ?, ?, ?)', [results.insertId, req.body.to_enclosure_number, req.body.to_panel_no, req.body.to_port_number], function(error, results){
 					if(error){
-						console.log(error);
-						throw error;
+						db.rollback(function(){
+							console.log(error);
+						});
 					}
 					return;
 				});
